3f8614553358dcde9814aa68daa0b3f775a51634
Ben Crist <ben@magicmoremagic.com>
2017-07-03 21:48:32 -0500

    add limp_src to build script

--- build.lua
+++ build.lua
@@ -1,6 +1,7 @@
 demo 'd-misc' {
    app 'consolecolors' {
       icon 'icon/bengine.ico',
+      limp_src 'src-consolecolors/*.hpp',
       src 'src-consolecolors/*.cpp',
       link_project { 'core', 'core-id' }
    }
